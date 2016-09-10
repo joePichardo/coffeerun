aea723c598a91d3374886ec1835d9fd3bf53eec6
joePichardo <joepichar@gmail.com>
2016-09-09 23:07:41 -0700

    Added CheckList object, handlers, and functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,26 @@
 (function (window) {
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
 
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
 
   var myTruck = new Truck('HAL', new DataStore());
+  window.myTruck = myTruck; // global namespace
+
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(FORM_SELECTOR); // makes FormHandler work with DOM element
 
-  // binds myTruck instance to guarantee it is the same owner
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck)); // sets up sumbit listener
+  // 'call' myTruck.createOrder and checkList.addRow through an anonymous function
+  formHandler.addSubmitHandler(function (data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  }); // sets up sumbit listener
   console.log(formHandler);
 
-  window.myTruck = myTruck; // global namespace
 })(window);
