2a83efc0728a97c36984fcbc8cf60adf73ae2bee
joePichardo <joepichar@gmail.com>
2016-09-11 11:37:08 -0700

    Adding script and elements for validating the form

---
+++ scripts/validation.js
@@ -0,0 +1,14 @@
+/* IIFE - immediately invoked function expression */
+(function(window) {
+  'use strict';
+  var App = window.App || {}; // if there is an App assign it, else make a new object
+
+  var Validation = {
+    isCompanyEmail: function (email) {
+      return /.+@bignerdranch\.com$/.test(email);
+    }
+  };
+
+  App.Validation = Validation; // attached Validation to the App object
+  window.App = App; // reassigned global App property
+})(window);
